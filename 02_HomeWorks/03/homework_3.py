6809d83412073a2c2b537a9985ed1b16beb2d2b5
Maximus7924 <nelin.maxim@gmail.com>
2024-07-26 14:24:18 +1000

    Update homework_3.py
    комментарий к перезаписи переменной "age"

--- 02_HomeWorks/03/homework_3.py
+++ 02_HomeWorks/03/homework_3.py
@@ -5,5 +5,8 @@ print("Age :",age)
 next_age = 45
 age = next_age
 print("Next age :",age)
+
+# возможно имелась в виду конструкция вида print(age +1) немного недопонял условия так как реализация в коде по сути присвоение ( которое указана как не неадо делать в задании )
+
 is_student = True
 print("Is student :",is_student)
