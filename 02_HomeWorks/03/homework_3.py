33db704ddb8650a26d9b8e21bd943fe8c77cd0be
¨Maximus7924¨ <nelin.maxim@gmail.com>
2024-07-26 19:16:55 +1000

    исправлена конструкция расчёта "Next age"

--- 02_HomeWorks/03/homework_3.py
+++ 02_HomeWorks/03/homework_3.py
@@ -2,11 +2,6 @@ name = "Maxim"
 print("Name :",name)
 age = 44
 print("Age :",age)
-next_age = 45
-age = next_age
-print("Next age :",age)
-
-# возможно имелась в виду конструкция вида print(age +1) немного недопонял условия так как реализация в коде по сути присвоение ( которое указана как не неадо делать в задании )
-
+print("Next age :",age+1)
 is_student = True
 print("Is student :",is_student)
