10e59f739448b851f004e5037bbd0d28c01c8046
¨Maximus7924¨ <nelin.maxim@gmail.com>
2024-07-26 19:24:25 +1000

    исправлена конструкция расчёта "Next age"

--- 02_HomeWorks/03/homework_3.py
+++ 02_HomeWorks/03/homework_3.py
@@ -1,7 +1,7 @@
 name = "Maxim"
-print("Name :",name)
+print("Name :", name)
 age = 44
-print("Age :",age)
-print("Next age :",age+1)
+print("Age :", age)
+print("Next age :", age+1)
 is_student = True
-print("Is student :",is_student)
+print("Is student :", is_student)
