832871c578e6303f40d4a412c52637848b25cc15
¨Maximus7924¨ <nelin.maxim@gmail.com>
2024-07-26 11:25:05 +1000

    Старт проекта "Домашнее задание"

---
+++ 01_Intro/02/intro_2_task3.py
@@ -0,0 +1,23 @@
+# 3rd program
+a = 1234
+b = 5678
+
+a1 = a % 1000
+print(a1)
+a2 = a % 10
+print(a2)
+a3 = a1 - a2
+print(a3)
+a4 = a3 // 10
+print(a4)
+
+b1 = b % 1000
+print(b1)
+b2 = b % 10
+print(b2)
+b3 = b1 - b2
+print(b3)
+b4 = b3 // 10
+print(b4)
+
+print(a4+b4)
---
+++ 02_HomeWorks/02/homework2.py
@@ -0,0 +1,9 @@
+home_works = 12
+time_spent = 1.5
+course_name = 'Python'
+task_exec_time = (time_spent/home_works)
+
+print('Курс: ', course_name,
+      ',\n всего задач: ', home_works,
+      ',\n затрачено часов: ', time_spent,
+      ',\n среднее время выполнения ', task_exec_time, ' часа.', sep='')
