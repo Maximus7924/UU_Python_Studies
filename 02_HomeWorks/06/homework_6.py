4ccaeda4aec22221f8dc3fc9a041ba05e6d86e51
¨Maximus7924¨ <nelin.maxim@gmail.com>
2024-07-30 00:36:42 +1000

    Домашняя работа 6 ( добавил заголовки к строкам )

--- 02_HomeWorks/06/homework_6.py
+++ 02_HomeWorks/06/homework_6.py
@@ -1,19 +1,19 @@
 print('Словарь\n')
 my_dict = {'Юля': 1977, 'Максим': 1979, 'Даша': 2005, 'Настя': 2011}
-print(my_dict)
-print(my_dict.get('Юля'))
-print(my_dict.pop('Бабушка', 'нет данных'))
+print('Начальный словарь :', my_dict)
+print("Значение ключа 'Юля' :", my_dict.get('Юля'))
+print("Поиск ключа 'Бабушка' :", my_dict.pop('Бабушка', 'нет данных'))
 my_dict.update({'Мама': 1959, 'Папа': 1961, })
-print(my_dict)
+print('Обновлённый словарь :', my_dict)
 father = my_dict.pop('Папа')
-print(father)
-print(my_dict)
+print("Значение вытащенного из словаря ключа 'Папа' :", father)
+print('Итоговый слловарь :', my_dict)
 
 print('\nМножества\n')
 my_set = {1, 9, 7, 7, 1, 9, 7, 9, 'Мама'}
-print(my_set)
+print('Множество :', my_set)
 my_set_add = {6, 8}
 my_set.update(my_set_add)
-print(my_set)
+print('Обновлённое множество :', my_set)
 my_set.remove('Мама')
-print(my_set)
+print('Итоговое множество с удалённым элементом :', my_set)
