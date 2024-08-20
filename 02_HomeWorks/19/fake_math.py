dcc5ccb8c564c551453cc4528356332d942186e1
¨Maximus7924¨ <nelin.maxim@gmail.com>
2024-08-20 11:22:43 +1000

    "доработка кода"

--- 02_HomeWorks/19/fake_math.py
+++ 02_HomeWorks/19/fake_math.py
@@ -1,5 +1,5 @@
 def divide(first, second):
-	ans = 0
+	# ans = 0 # в принципе то и не нужно тут объявлять переменную она создаётся и сразу получает вычисляемое значение
 	if second != 0:
 		ans = first / second
 	else:
--- 02_HomeWorks/19/true_math.py
+++ 02_HomeWorks/19/true_math.py
@@ -1,9 +1,9 @@
 from math import inf
 
 def divide(first, second):
-	ans = 0
+	# ans = 0 # в принципе то и не нужно тут объявлять переменную она создаётся и сразу получает вычисляемое значение
 	if second != 0:
 		ans = first / second
 	else:
-		ans = inf
+		return inf
 	return ans
