8e4d3b55e3c07a2d3ed4f7bbbad73693a145e26b
¨Maximus7924¨ <nelin.maxim@gmail.com>
2024-08-20 13:57:11 +1000

    доработанно согласно указаниям преподавателя

--- 02_HomeWorks/19/fake_math.py
+++ 02_HomeWorks/19/fake_math.py
@@ -1,7 +1,9 @@
 def divide(first, second):
 	# ans = 0 # в принципе то и не нужно тут объявлять переменную она создаётся и сразу получает вычисляемое значение
-	if second != 0:
-		ans = first / second
+	if second == 0:
+		return 'Ошибка'
 	else:
-		ans = 'Ошибка'
+		ans = first / second
 	return ans
+
+# print(divide(4, 0))
--- 02_HomeWorks/19/true_math.py
+++ 02_HomeWorks/19/true_math.py
@@ -2,8 +2,10 @@ from math import inf
 
 def divide(first, second):
 	# ans = 0 # в принципе то и не нужно тут объявлять переменную она создаётся и сразу получает вычисляемое значение
-	if second != 0:
-		ans = first / second
-	else:
+	if second == 0:
 		return inf
+	else:
+		ans = first / second
 	return ans
+
+# print(divide(5, 0))
